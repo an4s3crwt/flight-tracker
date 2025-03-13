94e690e2808e61e12ea9d799ed2b539c812c0d1c
Ana Gilabert Lucas <anaymenta@gmail.com>
2025-03-13 12:11:03 +0100

    Crea el archivo App.jsx dentro del directorio pages. Agrega una estructura básica al archivo App.jsx

---
+++ flight/src/pages/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to React Flight Tracker</h1>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
